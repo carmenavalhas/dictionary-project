5feb1d0d0578643c53241e9ff8c62912eafc8be5
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-06-29 17:10:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './cn-logo.png';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo
+        img-fluid" alt="logo" />
+      
+      </header>
+    </div>
+  );
+}
+
