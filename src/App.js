a420020b76c0bff59561a51f81295eb74eb31149
Carmen <38879535+carmenavalhas@users.noreply.github.com>
2023-07-02 10:51:15 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './cn-logo.png';
 import 'bootstrap/dist/css/bootstrap.css';
+import Dictionary from './Dictionary';
 
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
+
       <header className="App-header">
         <img src={logo} className="App-logo
         img-fluid" alt="logo" />
-      
+   
       </header>
+      <main>
+        <Dictionary />
+      </main>
+       <footer className='App-footer'>
+        <small>Coded by Carmen Navalhas</small>
+    </footer>
+    
+    </div>
     </div>
   );
 }
